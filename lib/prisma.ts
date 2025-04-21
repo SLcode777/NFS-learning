8fe4b9cc65d1e2e4177f5abe04191b6d08642505
melvynxdev <melvynmal@gmail.com>
2025-04-21 12:54:26 +0800

    upgrade to latest version of stripe

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
-// lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+// lib/prisma.
+import { PrismaClient } from "@/lib/generated/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
